abd6ee3c580285734b30c77afad5601f58580b79
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-04-12 20:26:42 +0900

    [melon] home 화면에 animation 이용하여 플레이어 만들기

--- src/client/js/songPlayer.js
+++ src/client/js/songPlayer.js
@@ -1,5 +1,27 @@
+const song = document.querySelector("audio");
+const lp = document.getElementById("lpRotate");
 const songContainer = document.getElementById("songContainer");
 const songTitle = document.getElementById("songTitle");
+const homePlayBtn = document.getElementById("homePlayBtn");
+
+
+const handleHomePlay = (e) => {
+    if (song.paused) {
+        song.volume = 0.5;
+        song.play();
+        lp.style.display='block'
+    }
+}
+
+const handleLpClick = (e) => {
+    if (lp.style.display='block') {
+        song.pause();
+        lp.style.display='none'
+    } 
+}
+
+
+
 
 const handleSongClick = () => {
     const { id } = songContainer.dataset;
@@ -8,4 +30,6 @@ const handleSongClick = () => {
     });
 };
 
+lp.addEventListener("click", handleLpClick);
+homePlayBtn.addEventListener("click", handleHomePlay);
 songTitle.addEventListener("click", handleSongClick);
